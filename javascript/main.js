d34313e42a4f3107c45b2d3b4f931603ea876eb0
jose <josemiguelojedahernandez77@gmail.com>
2026-03-09 16:11:51 +0000

    Add main al Index

--- javascript/main.js
+++ javascript/main.js
@@ -16,18 +16,17 @@ async function cargarDatos() {
 
         // 3. Renderizamos las partes de la página
         if (document.getElementById('site-header')) {
-            await renderizarHeader(datos.configuracion); // También le ponemos await por si acaso
+            await renderizarHeader(datos.configuracion);
         }
 
-        /*
         if (document.getElementById('main-content')) {
-            renderizarMain(datos.talleres);
+           await renderizarMain();
         }
 
         if (document.getElementById('site-footer')) {
             renderizarFooter(datos.footer);
         }
-        */
+
     } catch (error) {
         console.error("Fallo en el Corazón de Datos:", error);
     }
@@ -55,12 +54,61 @@ async function renderizarHeader(config) {
             logo.alt = `Logo de ${config.nombreSitio}`;
         }
 
-        const imagenPerfil = document.getElementById('Taller-image');
-        if (imagenPerfil) {
-            imagenPerfil.src = "../Fotos/mapa.png";
-        }
 
     } catch (error) {
         console.error("Error al pintar el header:", error);
     }
+}
+
+async function renderizarMain() {
+
+    try {
+
+        const respuesta = await fetch('../partials/Talleres-Previsualizacion.html');
+        if (!respuesta.ok) throw new Error("Error cargando el HTML del header");
+        const main = await respuesta.text();
+
+        const respuestaJson = await fetch('../json/content.json');
+        if (!respuestaJson.ok) throw new Error("Error cargando el JSON");
+        const datosGenerales = await respuestaJson.json();
+
+        const talleres = datosGenerales.talleres.slice(0,2);
+
+        const mainContainer = document.getElementById('main-content');
+
+        let mainfinal = '<div class="tu-contenedor-flex">';
+
+        talleres.forEach(taller => {
+            const numeroResenas = taller.reviews.length;
+            let promedio = 0;
+
+            if (numeroResenas > 0) {
+                const sumaEstrellas = taller.reviews.reduce((suma, review) => suma + review.stars, 0);
+                promedio = Math.round(sumaEstrellas / numeroResenas);
+            }
+
+            const estrellasVisuales = '★'.repeat(promedio) + '☆'.repeat(5 - promedio);
+
+            let tarjetaTaller = main
+                .replaceAll('{{id}}', taller.id)
+                .replaceAll('{{nombre}}', taller.name)
+                .replaceAll('{{imagen}}', taller.image[0]) // Tomamos la 1ra imagen del array
+                .replaceAll('{{resenas}}', numeroResenas)
+                .replaceAll('★★★★★', estrellasVisuales); // Cambiamos las fijas por las dinámicas
+
+            mainfinal += tarjetaTaller;
+
+        });
+
+        mainfinal += '</div>';
+        mainContainer.innerHTML = mainfinal;
+
+    } catch (error) {
+        console.error("Error al pintar el main:", error);
+    }
+
+}
+
+async function renderizarFooter(datos) {
+
 }
