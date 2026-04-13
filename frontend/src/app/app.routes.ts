8539e2c2085ad96ede91948618502f9ddfad8a5d
jose <josemiguelojedahernandez77@gmail.com>
2026-04-13 15:49:53 +0000

    Header refactorizado

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TallerInformacion } from './pages/TalleresInformacion/TallerInformacion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'TallerInfo', component: TallerInformacion },
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject, OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import{ HeaderComponent } from './components/header/header';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class App implements OnInit {
+  protected readonly title = signal('BuscaTaller');
+
+  private router = inject(Router);
+
+  public showLayout = signal(true);
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const noLayoutRoutes = ['/login'];
+        const currentUrl = event.urlAfterRedirects.split('?')[0];
+        this.showLayout.set(!noLayoutRoutes.includes(currentUrl));
+      });
+  }
 }
