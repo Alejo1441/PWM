22f3f764a6e4ab70632d62e5affcb0db9a4aec74
jose <josemiguelojedahernandez77@gmail.com>
2026-04-14 19:17:29 +0000

    Footer refactorizado

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal, inject, OnInit } from '@angular/core';
 import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import{ HeaderComponent } from './components/header/header';
+import { FooterComponent } from './components/footer/footer';
 import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent, FooterComponent],
   templateUrl: './app.html',
 })
 export class App implements OnInit {
